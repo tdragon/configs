8f0be134f895f99ec80d75cd0b3ec038b462f84a
tdragon <1210261+tdragon@users.noreply.github.com>
2024-03-23 09:38:21 +0200

    Tweak wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,8 +11,21 @@ config.window_background_opacity = 0.90
 config.font = wezterm.font("Hack Nerd Font", {bold=false, italic=false})
 config.font_size = 15.0
 
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
 --- Fix Alt keys for FI layout
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
+
+-- https://wezfurlong.org/wezterm/config/lua/gui-events/gui-startup.html
+-- Maximize on start
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 
 return config
