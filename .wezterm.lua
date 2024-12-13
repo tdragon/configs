faf8b2c37fa34969ace3394dc37a136b332e50aa
tdragon <1210261+tdragon@users.noreply.github.com>
2024-12-13 15:24:33 +0200

    feat: update configurations and add new aliases
    - Added `git_clean` alias to `.aliases` for cleaning up merged branches.
    - Updated `aws_login` function in `.aliases` to export `AWS_PROFILE`.
    - Added new device configuration in `karabiner.json`.
    - Unbound `Ctrl q` in `zellij` configuration.
    - Changed color scheme and updated font settings in `wezterm.lua`.

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,18 +5,25 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "nord"
+config.color_scheme = "Tokyo Night Moon"
+-- config.color_scheme = "Catppuccin Mocha"
+
 -- config.window_background_opacity = 0.90
 -- config.window_background_image = wezterm.home_dir .. "/terminal-background.png"
 
 config.font = wezterm.font_with_fallback({
-	{ family = "Iosevka Term", weight = "Medium" },
+	{ family = "Iosevka Term" },
 	-- { family = "JetBrains Mono", weight = "Medium" },
 	"Nerd Font Symbols",
 	"Noto Color Emoji",
 })
 
-config.font_size = 18.0
+config.font_size = 18
+config.line_height = 1.0
+-- config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
+config.cell_width = 1.0
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
